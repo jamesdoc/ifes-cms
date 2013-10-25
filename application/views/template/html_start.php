007ce77025c0539ca628b53ecf536d8e449af285
James Doc <jamesdoc@gmail.com>
2013-10-25 12:11:17 +0100

    IFES logo in the banner

--- application/views/template/html_start.php
+++ application/views/template/html_start.php
@@ -33,13 +33,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">IFES CMS</a>
+      <a class="navbar-brand" href="<?=site_url()?>"><img src="<?=site_url('assets/images/logo.png')?>" height="18" /></a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="<?=site_url()?>">Home</a></li>
+        <li><a href="<?=site_url('blog')?>">Blog</a></li>
+        <li><a href="<?=site_url('event')?>">Events</a></li>
       </ul>
 
       <ul class="nav navbar-nav navbar-right">
