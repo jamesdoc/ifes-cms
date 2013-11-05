16b9c42bef902edb38e87ad552bb4d6d57e3e409
James Doc <jamesdoc@gmail.com>
2013-11-05 10:26:25 +0000

    moved sidebar actions to own template file

---
+++ application/views/template/goto_sidebar.php
@@ -0,0 +1,23 @@
+<div class="panel panel-primary">
+
+	<div class="panel-heading">
+
+		<h3 class="panel-title">Go to...</h3>
+
+	</div>
+
+	<div class="panel-body">
+		
+		<p><a href="<?=site_url('blog')?>">Blogs</a>
+		<p><a href="<?=site_url('prayer')?>">Prayer Diary</a>
+		<p><a href="<?=site_url('event')?>">Events</a>
+		<p><a href="<?=site_url('profile')?>">Profiles</a>
+		<p><a href="<?=site_url('video')?>">Video</a>
+
+		<hr />
+
+		<p><a href="<?=site_url('comment')?>">Comment moderation</a>
+
+	</div>
+	
+</div>
